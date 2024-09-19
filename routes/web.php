729d784a3974dcc93173fc1199b63cfc9b19ed67
thauveel <thauvyl@gmail.com>
2024-09-20 02:19:04 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::name('front.')->group(function () {
         \Illuminate\Support\Facades\Artisan::call('route:cache');
         \Illuminate\Support\Facades\Artisan::call('view:clear');
         \Illuminate\Support\Facades\Artisan::call('storage:link');
-        \Illuminate\Support\Facades\Artisan::call('migrate');
+        \Illuminate\Support\Facades\Artisan::call('migrate:fresh');
         \Illuminate\Support\Facades\Artisan::call('db:seed DatabaseSeeder');
         
     

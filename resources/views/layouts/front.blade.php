a77d5e392d4cab900bf458afc76c378276a57cde
thauveel <thauvyl@gmail.com>
2024-09-21 14:38:25 +0500

    fix(footer):

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -72,26 +72,7 @@
 
     <footer class="bg-white">
         <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-            <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
-            <!-- <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">About</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Jobs</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Press</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Accessibility</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Partners</a>
-            </div> -->
-            </nav>
+            
             <div class="mt-10 flex justify-center space-x-10">
             <a href="#" class="text-gray-400 hover:text-gray-500">
                 <span class="sr-only">Facebook</span>
@@ -124,7 +105,7 @@
                 </svg>
             </a>
             </div>
-            <ped class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2020 Raa Atoll Council Inc. All rights reserved. Devloped by Thauveel</p>
+            <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2020 Raa Atoll Council Inc. All rights reserved.</p>
         </div>
     </footer>
     

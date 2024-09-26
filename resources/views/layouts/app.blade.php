7cb767aaae04435a9a0340e2e058bb52c6cea973
thauveel <thauvyl@gmail.com>
2024-09-26 12:26:32 +0500

    fix(seo):

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="UCVL ">
-        <meta name="title" content="UCVL ">
-        <meta name="author" content="UCVL ">
+        <meta name="description" content="Tournament Manager ">
+        <meta name="title" content="TM ">
+        <meta name="author" content="Thauveel ">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="generator" content="UC">
         <link rel="apple-touch-icon" sizes="57x57" href="/img/fav/apple-icon-57x57.png">
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="UCVL ">
-    <meta name="title" content="UCVL ">
-    <meta name="author" content="UCVL ">
+    <meta name="description" content="Tournament Manager ">
+    <meta name="title" content="TM ">
+    <meta name="author" content="Thauveel ">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="generator" content="UC">
     <link rel="apple-touch-icon" sizes="57x57" href="/img/fav/apple-icon-57x57.png">
